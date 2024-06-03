fde19eed6c69abdf3a1246bd9ac961fc5c7fc7fc
ArturHeBraga <artur.hebraga@outlook.com>
2024-06-02 21:16:30 -0300

    m

--- SafeHome/obj/Release/net8.0/SafeHome.AssemblyInfo.cs
+++ SafeHome/obj/Release/net8.0/SafeHome.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeHome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b920f4fadb83e25241b9fcec827e46683912eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686e35eb3dd7f987697b9909710364f867c0d51a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SafeHome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeHome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
