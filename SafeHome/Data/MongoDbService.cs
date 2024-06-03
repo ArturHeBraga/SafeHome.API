2c6cfa5c828bc75790e2f5c716ae95fca03b5376
ArturHeBraga <artur.hebraga@outlook.com>
2024-06-02 23:12:33 -0300

    mongo db services

--- SafeHome/Data/MongoDbService.cs
+++ SafeHome/Data/MongoDbService.cs
@@ -1,22 +1,34 @@
 ﻿using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+
 namespace SafeHome.Data
 {
-
     public class MongoDbService
     {
         private readonly IConfiguration _configuration;
-        private readonly IMongoDatabase? _database;
+        private readonly IMongoDatabase _database;
+
         public MongoDbService(IConfiguration configuration)
         {
             _configuration = configuration;
 
             var connectionString = _configuration.GetConnectionString("DbConnection");
-            var mongoUrl = MongoUrl.Create(connectionString);
-            var mongoClient = new MongoClient(mongoUrl);
-            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+            var databaseName = _configuration["ConnectionStrings:DatabaseName"];
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString), "Connection string cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentNullException(nameof(databaseName), "Database name cannot be null or empty");
+            }
+
+            var mongoClient = new MongoClient(connectionString);
+            _database = mongoClient.GetDatabase(databaseName);
         }
 
-        public IMongoDatabase? Database => _database;
-
+        public IMongoDatabase Database => _database;
     }
 }
