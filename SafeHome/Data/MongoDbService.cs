87222c7baa9b97ffdba5c9ef4121ac09e9768d77
ArturHeBraga <artur.hebraga@outlook.com>
2024-06-02 23:46:51 -0300

    tentativa numero 10000000

--- SafeHome/Data/MongoDbService.cs
+++ SafeHome/Data/MongoDbService.cs
@@ -1,19 +1,17 @@
 ﻿using MongoDB.Driver;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace SafeHome.Data
 {
     public class MongoDbService
     {
-        private readonly IConfiguration _configuration;
         private readonly IMongoDatabase _database;
 
         public MongoDbService(IConfiguration configuration)
         {
-            _configuration = configuration;
-
-            var connectionString = _configuration.GetConnectionString("DbConnection");
-            var databaseName = _configuration["ConnectionStrings:DatabaseName"];
+            var connectionString = configuration.GetConnectionString("DbConnection");
+            var databaseName = configuration["ConnectionStrings:DatabaseName"];
 
             if (string.IsNullOrEmpty(connectionString))
             {
